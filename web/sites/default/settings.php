d4ea30eff67f5520c5d366ce5dff00e36dd43668
Veronica Severyn <veranika@inclind.com>
2018-06-26 07:45:33 -0400

    after composer update

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -38,14 +38,3 @@ if (file_exists($local_settings)) {
  * See: tests/installer-features/installer.feature
  */
 $settings['install_profile'] = 'social';
-$databases['default']['default'] = array (
-  'database' => 'drupal8',
-  'username' => 'drupal8',
-  'password' => 'drupal8',
-  'prefix' => '',
-  'host' => 'database',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'I4sOi6aIx1sFuEdJC_FAGw-Bv9_Cr8uPEWZCm8P4XVv2yojSJs5-dm4SrMvR84VdbHoXK12GEQ';
