c8975a98be867b2edbd0c82f25f28b3b7fd1f6bc
Veronica Severyn <veranika@inclind.com>
2018-06-26 07:11:00 -0400

    after composer update

--- web/modules/contrib/r4032login/src/EventSubscriber/R4032LoginSubscriber.php
+++ web/modules/contrib/r4032login/src/EventSubscriber/R4032LoginSubscriber.php
@@ -2,21 +2,24 @@
 
 namespace Drupal\r4032login\EventSubscriber;
 
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\EventSubscriber\HttpExceptionSubscriberBase;
+use Drupal\Core\Path\PathMatcherInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Routing\RedirectDestinationInterface;
 use Drupal\Core\Url;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\r4032login\Event\RedirectEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Drupal\Component\Utility\Xss;
 
 /**
  * Redirect 403 to User Login event subscriber.
  */
-class R4032LoginSubscriber implements EventSubscriberInterface {
+class R4032LoginSubscriber extends HttpExceptionSubscriberBase {
 
   /**
    * The configuration factory.
@@ -40,21 +43,46 @@ class R4032LoginSubscriber implements EventSubscriberInterface {
   protected $redirectDestination;
 
   /**
+   * The path matcher.
+   *
+   * @var \Drupal\Core\Path\PathMatcherInterface
+   */
+  protected $pathMatcher;
+
+  /**
+   * An event dispatcher instance to use for map events.
+   *
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+  protected $eventDispatcher;
+
+  /**
    * Constructs a new R4032LoginSubscriber.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The configuration factory.
-   * @param \Drupal\Core\Routing\UrlGeneratorInterface $url_generator
-   *   The url generator service.
    * @param \Drupal\Core\Session\AccountInterface $current_user
    *   The current user.
    * @param \Drupal\Core\Routing\RedirectDestinationInterface $redirect_destination
    *   The redirect destination service.
+   * @param \Drupal\Core\Path\PathMatcherInterface $path_matcher
+   *   The path matcher.
+   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
+   *   The event dispatcher.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, AccountInterface $current_user, RedirectDestinationInterface $redirect_destination) {
+  public function __construct(ConfigFactoryInterface $config_factory, AccountInterface $current_user, RedirectDestinationInterface $redirect_destination, PathMatcherInterface $path_matcher, EventDispatcherInterface $event_dispatcher) {
     $this->configFactory = $config_factory;
     $this->currentUser = $current_user;
     $this->redirectDestination = $redirect_destination;
+    $this->pathMatcher = $path_matcher;
+    $this->eventDispatcher = $event_dispatcher;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getHandledFormats() {
+    return ['html'];
   }
 
   /**
@@ -62,64 +90,97 @@ class R4032LoginSubscriber implements EventSubscriberInterface {
    *
    * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
    *   The Event to process.
-   *
-   * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
-   *   Thrown when the access is denied and redirects to user login page.
    */
-  public function onKernelException(GetResponseEvent $event) {
-    $exception = $event->getException();
-    if (!($exception instanceof AccessDeniedHttpException)) {
-      return;
-    }
+  public function on403(GetResponseEvent $event) {
     $config = $this->configFactory->get('r4032login.settings');
-    $options = array();
-    $options['query'] = $this->redirectDestination->getAsArray();
-    $options['absolute'] = TRUE;
-    $code = $config->get('default_redirect_code');
-    if ($this->currentUser->isAnonymous()) {
-      // Show custom access denied message if set.
-      if ($config->get('display_denied_message')) {
-        $message = $config->get('access_denied_message');
-        $message_type = $config->get('access_denied_message_type');
-        drupal_set_message(Xss::filterAdmin($message), $message_type);
+
+    // Check if the path should be ignored.
+    $noRedirectPages = trim($config->get('match_noredirect_pages'));
+    if ($noRedirectPages !== '') {
+      $pathToMatch = $this->redirectDestination->get();
+
+      try {
+        // Clean up path from possible language prefix, GET arguments, etc.
+        $pathToMatch = '/' . Url::fromUserInput($pathToMatch)->getInternalPath();
       }
-      // Handle redirection to the login form.
-      $login_path = $config->get('user_login_path');
-      $url = Url::fromUserInput($login_path, $options)->toString();
-      $response = new RedirectResponse($url, $code);
-      $event->setResponse($response);
+      catch (\Exception $e) {
+      }
+
+      if ($this->pathMatcher->matchPath($pathToMatch, $noRedirectPages)) {
+        return;
+      }
+    }
+
+    // Retrieve the redirect path depending if the user is logged or not.
+    if ($this->currentUser->isAnonymous()) {
+      $redirectPath = $config->get('user_login_path');
     }
     else {
-      // Check to see if we are to redirect the user.
-      $redirect = $config->get('redirect_authenticated_users_to');
-      if ($redirect) {
-        // Custom access denied page for logged in users.
-        if ($redirect === '<front>') {
+      $redirectPath = $config->get('redirect_authenticated_users_to');
+    }
+
+    if (!empty($redirectPath)) {
+      // Determine if the redirect path is external.
+      $externalRedirect = UrlHelper::isExternal($redirectPath);
+
+      // Determine the HTTP redirect code.
+      $code = $config->get('default_redirect_code');
+
+      // Determine the url options.
+      $options = [
+        'absolute' => TRUE,
+      ];
+
+      // Determine the destination parameter
+      // and add it as options for the url build.
+      if ($config->get('redirect_to_destination')) {
+        $destination = $this->redirectDestination->get();
+
+        if ($externalRedirect) {
+          $destination = Url::fromUserInput($destination, [
+            'absolute' => TRUE,
+          ])->toString();
+        }
+
+        if (empty($config->get('destination_parameter_override'))) {
+          $options['query']['destination'] = $destination;
+        }
+        else {
+          $options['query'][$config->get('destination_parameter_override')] = $destination;
+        }
+      }
+
+      // Allow to alter the url or options before to redirect.
+      $redirectEvent = new RedirectEvent($redirectPath, $options);
+      $this->eventDispatcher->dispatch(RedirectEvent::EVENT_NAME, $redirectEvent);
+      $redirectPath = $redirectEvent->getUrl();
+      $options = $redirectEvent->getOptions();
+
+      // Perform the redirection.
+      if ($externalRedirect) {
+        $url = Url::fromUri($redirectPath, $options)->toString();
+        $response = new TrustedRedirectResponse($url);
+      }
+      else {
+        // Show custom access denied message if set.
+        if ($this->currentUser->isAnonymous() && $config->get('display_denied_message')) {
+          $message = $config->get('access_denied_message');
+          $messageType = $config->get('access_denied_message_type');
+          drupal_set_message(Xss::filterAdmin($message), $messageType);
+        }
+
+        if ($redirectPath === '<front>') {
           $url = \Drupal::urlGenerator()->generate('<front>');
         }
         else {
-          $url = Url::fromUserInput($redirect, $options)->toString();
+          $url = Url::fromUserInput($redirectPath, $options)->toString();
         }
 
         $response = new RedirectResponse($url, $code);
-        $event->setResponse($response);
       }
-      else {
-        // Display the default access denied page.
-        return new AccessDeniedHttpException();
-      }
-    }
-  }
 
-  /**
-   * {@inheritdoc}
-   *
-   * The priority for the exception must be as low as possible this subscriber
-   * to respond with AccessDeniedHttpException.
-   */
-  public static function getSubscribedEvents() {
-    $events[KernelEvents::EXCEPTION][] = array('onKernelException');
-    return $events;
+      $event->setResponse($response);
+    }
   }
 
 }
